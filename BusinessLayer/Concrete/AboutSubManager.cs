6d29e9ea7d817bf0ffb049f05bdce0db46402e7e
emredaglar <emre.daglar1@gmail.com>
2024-12-14 01:03:53 +0300

    added About

---
+++ BusinessLayer/Concrete/AboutSubManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+	public class AboutSubManager : IAboutSubService
+	{
+		IAboutSubDal _aboutSubDal;
+
+        public AboutSubManager(IAboutSubDal aboutSubDal)
+        {
+            _aboutSubDal = aboutSubDal;
+        }
+
+        public void TAdd(AboutSub entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(AboutSub entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AboutSub TGetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<AboutSub> TGetList()
+        {
+           return _aboutSubDal.GetList();   
+        }
+
+        public void TUpdate(AboutSub entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TraversalCoreProject/ViewComponents/Default/_AboutSubPartial.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.ViewComponents.Default
+{
+    public class _AboutSubPartial:ViewComponent
+    {
+        AboutSubManager aboutSubManager = new AboutSubManager(new EfAboutSubDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = aboutSubManager.TGetList();
+            return View(values);
+        }
+    }
+}

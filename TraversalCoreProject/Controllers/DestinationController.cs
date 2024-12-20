9f8c3dce9a13cb7227bfc8c683576fcfbbeab2e2
emredaglar <emre.daglar1@gmail.com>
2024-12-20 17:23:34 +0300

    added Destinaiton

--- TraversalCoreProject/Controllers/DestinationController.cs
+++ TraversalCoreProject/Controllers/DestinationController.cs
@@ -1,12 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace TraversalCoreProject.Controllers
 {
 	public class DestinationController : Controller
 	{
+		DestinationManager destinationManager=new DestinationManager(new EfDestinationDal());
 		public IActionResult Index()
 		{
-			return View();
+			var values=destinationManager.TGetList();
+			return View(values);
 		}
-	}
+        [HttpGet]
+        public IActionResult DestinationDetails(int id)
+        {
+         
+            return View();
+        }
+        [HttpPost]
+        public IActionResult DestinationDetails(Destination destination)
+        {
+
+            return View();
+        }
+    }
 }

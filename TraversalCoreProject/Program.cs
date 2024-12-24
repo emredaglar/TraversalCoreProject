2a610ed90159f2570e8b16387f382b1d45c10158
emredaglar <emre.daglar1@gmail.com>
2024-12-24 12:51:00 +0300

    added AdminTemplate and _AdminLayout

--- TraversalCoreProject/Program.cs
+++ TraversalCoreProject/Program.cs
@@ -52,6 +52,14 @@ app.UseEndpoints(endpoints =>
       pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
     );
 });
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+      name: "areas",
+      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    );
+});
+
 
 
 app.Run();

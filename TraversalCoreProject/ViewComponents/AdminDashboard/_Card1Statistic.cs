6ae4f85c4518b78ed687d223634a3b43c2a8974e
emredaglar <emre.daglar1@gmail.com>
2024-12-24 20:15:36 +0300

    added AdminDashboard

---
+++ TraversalCoreProject/ViewComponents/AdminDashboard/_Card1Statistic.cs
@@ -0,0 +1,17 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace TraversalCoreProject.ViewComponents.AdminDashboard
+{
+	public class _Card1Statistic:ViewComponent
+	{
+		Context context=new Context();
+		public IViewComponentResult Invoke()
+		{
+			ViewBag.v1 = context.Destinations.Count();
+			ViewBag.v2 = context.Users.Count();
+			return View();
+		}
+	}
+}

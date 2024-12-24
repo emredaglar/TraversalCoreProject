8187cb181df21c7730c1a2342a24416486a80f3a
emredaglar <emre.daglar1@gmail.com>
2024-12-24 12:30:04 +0300

    added MemberDashboard

---
+++ TraversalCoreProject/ViewComponents/MemberDashboard/_PlatformSetting.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.ViewComponents.MemberDashboard
+{
+    public class _PlatformSetting : ViewComponent
+    {
+        public IViewComponentResult Invoke() { return View(); }
+    }
+}

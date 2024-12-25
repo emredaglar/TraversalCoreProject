2a6b5df630f662b8d3740727a3f9e2b688e94e74
emredaglar <emre.daglar1@gmail.com>
2024-12-25 21:20:22 +0300

    added AJAX

---
+++ TraversalCoreProject/Areas/Admin/Controllers/CityController.cs
@@ -0,0 +1,72 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using TraversalCoreProject.Models;
+
+namespace TraversalCoreProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CityController : Controller
+    {
+        private readonly IDestinationService _destinationService;
+
+        public CityController(IDestinationService destinationService)
+        {
+            _destinationService = destinationService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CityList()
+        {
+            var jsonCity = JsonConvert.SerializeObject(_destinationService.TGetList());
+            return Json(jsonCity);
+        }
+
+        [HttpPost]
+        public IActionResult AddCityDestination([FromBody] Destination destination)
+        {
+            if (destination == null)
+            {
+                return BadRequest("Eksik veya hatalı veri gönderildi.");
+            }
+
+            destination.Status = true;
+            _destinationService.TAdd(destination);
+
+            var values = JsonConvert.SerializeObject(destination);
+            return Json(values);
+        }
+        [HttpGet]
+        public IActionResult GetById(int id)
+        {
+            var values = _destinationService.TGetById(id);
+            return Json(values);
+        }
+
+        public IActionResult DeleteCity(int id)
+        {
+            var values = _destinationService.TGetById(id);
+            _destinationService.TDelete(values);
+            return NoContent();
+        }
+
+        [HttpPost]
+        public IActionResult UpdateCity([FromBody] Destination destination)
+        {
+
+            if (string.IsNullOrEmpty(destination.City) || string.IsNullOrEmpty(destination.DayNight) || destination.DestinationId <= 0)
+            {
+                return Json(new { success = false, message = "Lütfen tüm alanları doldurun." });
+            }
+
+            _destinationService.TUpdate(destination);
+            Console.WriteLine("Güncelleme başarılı!");
+            return Json(new { success = true, message = "Şehir başarıyla güncellendi." });
+
+        }
+    }
+}

e67edb7626b571a69d667be0905e465e5ae657b7
emredaglar <emre.daglar1@gmail.com>
2024-12-25 16:16:36 +0300

    added Excel and PDF Services

--- BusinessLayer/Container/Extensions.cs
+++ BusinessLayer/Container/Extensions.cs
@@ -31,6 +31,9 @@ namespace BusinessLayer.Container
             services.AddScoped<IGuideService, GuideManager>();
             services.AddScoped<IGuideDal, EfGuideDal>();
 
-        }
+			services.AddScoped<IExcelService, ExcelManager>();
+
+			services.AddScoped<IPdfService, PdfManager>();
+		}
     }
 }

2b6683931bc13e1419424d26fbb038fba28dd05a
emredaglar <emre.daglar1@gmail.com>
2024-12-26 22:10:52 +0300

    added RapidApiMovie

---
+++ TraversalCoreProject/Areas/Admin/Controllers/ApiMovieController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using TraversalCoreProject.Areas.Admin.Models;
+
+namespace TraversalCoreProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ApiMovieController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            List<ApiMovieViewModel> apiMovies = new List<ApiMovieViewModel>();
+
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com/"),
+                Headers =
+    {
+        { "x-rapidapi-key", "3db79cb1e3mshfa9b8dd84816013p1e663fjsn48c308702bf9" },
+        { "x-rapidapi-host", "imdb-top-100-movies.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                apiMovies = JsonConvert.DeserializeObject<List<ApiMovieViewModel>>(body);
+                return View(apiMovies);
+            }
+
+        }
+
+    }
+}
+
---
+++ TraversalCoreProject/Areas/Admin/Models/ApiMovieViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace TraversalCoreProject.Areas.Admin.Models
+{
+    public class ApiMovieViewModel
+    {//apiden geldiği gibi olmalı
+        public int rank { get; set; }
+        public string title { get; set; }
+        public string rating { get; set; }
+        public int year { get; set; }
+        public string trailer { get; set; }
+        public string image { get; set; }
+        public string imdb_link { get; set; }
+    }
+}

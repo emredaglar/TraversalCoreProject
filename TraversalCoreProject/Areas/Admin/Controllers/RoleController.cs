0e32c21d9838ea9fb7eb0f550cb1d0ea17c4edbc
emredaglar <emre.daglar1@gmail.com>
2025-01-03 23:18:31 +0300

    added RoleList

---
+++ TraversalCoreProject/Areas/Admin/Controllers/RoleController.cs
@@ -0,0 +1,23 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class RoleController : Controller
+    {
+        private readonly RoleManager<AppRole> _roleManager;
+
+        public RoleController(RoleManager<AppRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            var values=_roleManager.Roles.ToList();
+            return View(values);
+        }
+    }
+}

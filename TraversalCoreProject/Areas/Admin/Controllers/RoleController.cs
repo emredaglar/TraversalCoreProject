4e838ad48441ac0d27ec9ae69ba03f83168c1c61
emredaglar <emre.daglar1@gmail.com>
2025-01-04 15:56:06 +0300

    added AssignRole

--- TraversalCoreProject/Areas/Admin/Controllers/RoleController.cs
+++ TraversalCoreProject/Areas/Admin/Controllers/RoleController.cs
@@ -9,10 +9,12 @@ namespace TraversalCoreProject.Areas.Admin.Controllers
 	public class RoleController : Controller
 	{
 		private readonly RoleManager<AppRole> _roleManager;
+		private readonly UserManager<AppUser> _userManager;
 
-		public RoleController(RoleManager<AppRole> roleManager)
+		public RoleController(RoleManager<AppRole> roleManager, UserManager<AppUser> userManager)
 		{
 			_roleManager = roleManager;
+			_userManager = userManager;
 		}
 
 		public IActionResult Index()
@@ -46,7 +48,7 @@ namespace TraversalCoreProject.Areas.Admin.Controllers
 		}
 		public async Task<IActionResult> DeleteRole(int id)
 		{
-			var value=_roleManager.Roles.FirstOrDefault(x => x.Id == id);
+			var value = _roleManager.Roles.FirstOrDefault(x => x.Id == id);
 			await _roleManager.DeleteAsync(value);
 			return RedirectToAction("Index");
 		}
@@ -68,6 +70,28 @@ namespace TraversalCoreProject.Areas.Admin.Controllers
 			value.Name = updateRoleViewModel.RoleName;
 			await _roleManager.UpdateAsync(value);
 			return RedirectToAction("Index");
+		}
+		public IActionResult UserList()
+		{
+			var values = _userManager.Users.ToList();
+			return View(values);
+		}
+		public async Task<IActionResult> AssignRole(int id)
+		{
+			var user=_userManager.Users.FirstOrDefault(x => x.Id == id);
+			var roles=_roleManager.Roles.ToList();
+			var userRoles = await _userManager.GetRolesAsync(user);
+			List<RoleAssignViewModel> roleAssignViewModels = new List<RoleAssignViewModel>();
+			foreach (var item in roles)
+			{
+				RoleAssignViewModel model = new RoleAssignViewModel();
+				model.RoleId = item.Id;
+				model.RoleName = item.Name;
+				model.RoleExist = userRoles.Contains(item.Name);
+				roleAssignViewModels.Add(model);
+					
+			}
+			return View(roleAssignViewModels);
 		}
 	}
 }
